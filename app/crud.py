1c5eb12175776b4c7a90fb3402b79d282f0e9cea
MiguelA-Zurita <mzurita@nigul.cide.es>
2025-03-28 19:19:56 +0100

    Finished update_task and delete_tasks methods

--- app/crud.py
+++ app/crud.py
@@ -30,19 +30,33 @@ def update_tasks(db: Session, task_id: int, task_update: TaskUpdate):
     Input:
         db: database session
     Output:
-        Updated some task fields
+        Updated task object or None if task not found
     """
-    # TODO: El vostre codi va aqui
-    stmnt = db.query(Task).where(Task.id == task_id)
-    pass
+    task_to_update = db.get(Task, task_id)
+    if not task_to_update:
+        return None  # Task not found
+
+    # Update the fields of the task
+    for key, value in task_update.model_dump(exclude_unset=True).items():
+        setattr(task_to_update, key, value)
+
+    db.commit()
+    db.refresh(task_to_update)
+    return task_to_update
 
 
 def delete_tasks(db: Session, task_id: int):
     """
     Input:
         db: database session
+        task_id: ID of the task to delete
     Output:
-        Return delete task
+        True if the task was deleted, False if not found
     """
-    # TODO: El vostre codi va aqui
-    Session.delete(task_id)
+    task_to_delete = db.get(Task, task_id)
+    if not task_to_delete:
+        return False  # Task not found
+
+    db.delete(task_to_delete)
+    db.commit()
+    return True
